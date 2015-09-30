ad2afb88e8220b95896f3b0c239b7f40b7e6c179
pvcary <pvcary@gmail.com>
2015-09-30 13:47:42 -0500

    Initial Commit

---
+++ IWantToBeGreen(IBOutlets,IBActions)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IWantToBeGreen(IBOutlets,IBActions)
+//
+//  Created by Stephen Cary on 9/30/15.
+//  Copyright © 2015 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IWantToBeGreen(IBOutlets,IBActions)/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IWantToBeGreen(IBOutlets,IBActions)
+//
+//  Created by Stephen Cary on 9/30/15.
+//  Copyright © 2015 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
